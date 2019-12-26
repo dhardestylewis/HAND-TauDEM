96c706536ce6538c5119e2c3a6336be1399dc792
Daniel Hardesty Lewis <dhl@tacc.utexas.edu>
2019-12-26 18:49:35 -0500

    Vis: 0m separated from 1.5m levels

--- scripts/hand-vis.py
+++ scripts/hand-vis.py
@@ -44,61 +44,62 @@ def main():
 
     options = argparser()
 
-    basindd = rasterio.open(options.dd)
-    basinddma = ma.array(basindd.read(),mask=(basindd.read()==basindd.nodata))
-    #basinddmasort = np.sort(ma.array(basinddma.data,mask=((basinddma.mask)|(basinddma.data==0.))).compressed())
-    basinddmasort = np.sort(basinddma.filled(fill_value=-1.).flatten())
+    dd = rasterio.open(options.dd)
+    ddma = ma.array(dd.read(),mask=(dd.read()==dd.nodata))
+    #ddmasort = np.sort(ma.array(ddma.data,mask=((ddma.mask)|(ddma.data==0.))).compressed())
+    nodata = -1.
+    ddmasort = np.sort(ddma.filled(fill_value=nodata).flatten())
 #    nbins=options.bins
-    basinddmasortbinsnodata = -1.
     binning = options.binmethod.split(" ")
     if binning[0] == 'lin':
         if len(binning)>1:
-            basinddmasortbins = np.linspace(
+            bins = np.linspace(
                 float(binning[1]),
                 float(binning[2]),
                 int(binning[3])
             )
         else:
-            basinddmasortbins = np.linspace(0.,1.5*5.,6)
-        basinddmasortbins = np.insert(basinddmasortbins,0,[basinddmasortbinsnodata])
-        basinddmasortbins = np.append(basinddmasortbins,basinddmasort.max())
+            bins = np.linspace(0.,1.5*5.,6)
+        bins = np.insert(bins,0,[nodata])
+        bins = np.append(bins,ddmasort.max())
     elif binning[0] == 'log':
         if len(binning)>1:
-            basinddmasortbins = np.logspace(
+            bins = np.logspace(
                 float(binning[1]),
                 float(binning[2]),
                 int(binning[3])
             )[:int(binning[4])]
         else:
-            basinddmasortbins = np.logspace(-1,3,10)[:7]
-        basinddmasortbins = np.insert(basinddmasortbins,0,[basinddmasortbinsnodata,0.])
-        basinddmasortbins = np.append(basinddmasortbins,basinddmasort.max())
-    #basinddmasortbins = np.logspace(-1,3,10)[:7]
-    #basinddmasortbins[len(basinddmasortbins)-1] = basinddmasort.max()
-    #basinddmasortbins = np.insert(basinddmasortbins,0,[basinddmasortbinsnodata,0.])
-    nbins = len(basinddmasortbins)-1
-#    basinddmasortbins = np.zeros(nbins+1)
-#    basinddmasortbins[0] = basinddmasort.min()
+            bins = np.logspace(-1,3,10)[:7]
+        bins = np.insert(bins,0,[nodata,0.])
+        bins = np.append(bins,ddmasort.max())
+    #bins = np.logspace(-1,3,10)[:7]
+    #bins[len(bins)-1] = ddmasort.max()
+    #bins = np.insert(bins,0,[nodata,0.])
+    nbins = len(bins) - 1
+#    bins = np.zeros(nbins+1)
+#    bins[0] = ddmasort.min()
 #    for i in range(1,nbins+1):
-#        basinddmasortbins[i] = basinddmasort[round(len(basinddmasort)/float(nbins)*float(i))-1]
-    ## basinddmasort[round(len(basinddmasort)/7.*7)-1]==basinddmasort.max()==True
-    #plt.hist(basinddmasort,bins=basinddmasortbins)
+#        bins[i] = ddmasort[round(len(ddmasort)/float(nbins)*float(i))-1]
+    ## ddmasort[round(len(ddmasort)/7.*7)-1]==ddmasort.max()==True
+    #plt.hist(ddmasort,bins=bins)
     ## "<=" in the first condition below
     ##  because we have intentionally excluded 0s as nodata values
-    basinddmasortbinsmean = np.zeros(nbins)
-    #basinddmasortbinsmean[1] = basinddmasort[(basinddmasortbins[1]<=basinddmasort)&(basinddmasort<=basinddmasortbins[2])].mean()
+    means = np.zeros(nbins)
+    #means[1] = ddmasort[(bins[1]<=ddmasort)&(ddmasort<=bins[2])].mean()
     for i in range(0,nbins):
-        basinddmasortbinsmean[i] = basinddmasort[(basinddmasortbins[i]<=basinddmasort)&(basinddmasort<basinddmasortbins[i+1])].mean()
-    basinddmasortbinscor = basinddmasortbins
-    basinddmasortbinscor[len(basinddmasortbinscor)-1] = basinddmasortbins[len(basinddmasortbins)-1]+1
-    basinddmadigi = np.digitize(basinddma.filled(fill_value=-1.),basinddmasortbinscor,right=False).squeeze()-1
-    basinddmadigiflat = basinddmadigi.flatten().astype(np.float32)
+        means[i] = ddmasort[(bins[i]<ddmasort)&(ddmasort<=bins[i+1])].mean()
+    binscor = bins
+#    binscor[len(binscor)-1] = bins[len(bins)-1] + 1
+    digi = np.digitize(ddma.filled(fill_value=nodata),binscor,right=True)
+    digi = digi.squeeze() - 1
+    digiflat = digi.flatten().astype(np.float32)
     for i in range(0,nbins):
-        basinddmadigiidc = np.where(basinddmadigi.flatten()==i)[0]
-        np.put(basinddmadigiflat,basinddmadigiidc,np.full(basinddmadigiidc.shape,basinddmasortbinsmean[i]))
-    basinddmadigirshp = basinddmadigiflat.reshape(basinddmadigi.shape)
-    shapes = list(rasterio.features.shapes(basinddmadigirshp,transform=basindd.transform))
-    unique_values = np.unique(basinddmadigirshp)
+        digiidc = np.where(digi.flatten()==i)[0]
+        np.put(digiflat,digiidc,np.full(digiidc.shape,means[i]))
+    digirshp = digiflat.reshape(digi.shape)
+    shapes = list(rasterio.features.shapes(digirshp,transform=dd.transform))
+    unique_values = np.unique(digirshp)
     shp_schema = {
         'geometry': 'MultiPolygon',
         'properties': {
@@ -107,41 +108,88 @@ def main():
             'bin_right': 'float'
         }
     }
-    vecshp = fiona.open(options.shapefile,'w','ESRI Shapefile', shp_schema, basindd.crs.to_string())
-    vecjsn = fiona.open(options.geojson,'w','GeoJSON', shp_schema, basindd.crs.to_string())
-    for pixel_value in unique_values:
+    vecshp = fiona.open(
+        options.shapefile,
+        'w',
+        'ESRI Shapefile',
+        shp_schema,
+        dd.crs.to_string()
+    )
+    vecjsn = fiona.open(
+        options.geojson,
+        'w',
+        'GeoJSON',
+        shp_schema,
+        dd.crs.to_string()
+    )
+    for i,pixel_value in enumerate(unique_values):
         polygons = [shape(geom) for geom,value in shapes if value==pixel_value]
         multipolygon = MultiPolygon(polygons)
-        vecshp.write({
-            'geometry': mapping(multipolygon),
-            'properties': {
-                'mean':float(pixel_value),
-                'bin_left': float(basinddmasortbins[i]),
-                'bin_right': float(basinddmasortbins[i+1])
-            }
-        })
-        vecjsn.write({
-            'geometry': mapping(multipolygon),
-            'properties': {
-                'mean':float(pixel_value),
-                'bin_left': float(basinddmasortbins[i]),
-                'bin_right': float(basinddmasortbins[i+1])
-            }
-        })
+        if i == 0:
+            vecshp.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[0]),
+                    'bin_right': float(bins[0])
+                }
+            })
+            vecjsn.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[0]),
+                    'bin_right': float(bins[0])
+                }
+            })
+        elif i == 1:
+            vecshp.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[1]),
+                    'bin_right': float(bins[1])
+                }
+            })
+            vecjsn.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[1]),
+                    'bin_right': float(bins[1])
+                }
+            })
+        else:
+            vecshp.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[i-1]),
+                    'bin_right': float(bins[i])
+                }
+            })
+            vecjsn.write({
+                'geometry': mapping(multipolygon),
+                'properties': {
+                    'mean': float(pixel_value),
+                    'bin_left': float(bins[i-1]),
+                    'bin_right': float(bins[i])
+                }
+            })
     vecshp.close()
     vecjsn.close()
 #    cmap = mpl.cm.get_cmap(options.cmap)
 #    cmapdiscretear = cmap(np.linspace(0,1,nbins))
 #    for i in range(1,nbins-1):
-#        cmapdiscretear[i] = np.append(cmap.colors[int(round(float(len(cmap.colors))/(basinddmasortbinsmean[nbins-1]-basinddmasortbinsmean[0])*np.cumsum(np.diff(basinddmasortbinsmean))[i-1]))-1],1.)
+#        cmapdiscretear[i] = np.append(cmap.colors[int(round(float(len(cmap.colors))/(means[nbins-1]-means[0])*np.cumsum(np.diff(means))[i-1]))-1],1.)
 #    cmapdiscretear = np.insert(cmapdiscretear,0,[0.,0.,0.,0.],axis=0)
 #    cmapdiscrete = mpl.colors.ListedColormap(cmapdiscretear)
 #    fig,ax = plt.subplots()
-#    cax = ax.imshow(basinddmadigi,interpolation='none',cmap=cmapdiscrete)
+#    cax = ax.imshow(digi,interpolation='none',cmap=cmapdiscrete)
 #    ax.set_title('Vertical distance down to nearest drainage')
 #    cbar = fig.colorbar(cax)
-#    basinddmasortbinsstr = ['{:.2f}'.format(x) for x in basinddmasortbins.tolist()]
-#    cbar.ax.set_yticklabels([s + ' m' for s in basinddmasortbinsstr])
+#    binsstr = ['{:.2f}'.format(x) for x in bins.tolist()]
+#    cbar.ax.set_yticklabels([s + ' m' for s in binsstr])
 #    plt.savefig(options.output)
 #    plt.show()
 

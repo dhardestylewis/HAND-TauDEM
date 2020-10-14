2feb49cd6f7833856c713b6dfbdfbabb7964a3a6
dhardestylewis <dhl@tacc.utexas.edu>
2020-10-14 12:21:52 -0500

    Update hand-2.1.0.sh

--- scripts/hand-2.1.0.sh
+++ scripts/hand-2.1.0.sh
@@ -2,15 +2,19 @@
 
 pitremove -z $1 -fel demfel.tif
 dinfflowdir -fel demfel.tif -ang demang.tif -slp demslp.tif
+## TODO: see about replacing all instances of d8 with dinf
 d8flowdir -fel demfel.tif -p demp.tif -sd8 demsd8.tif
 aread8 -p demp.tif -ad8 demad8.tif -nc
 areadinf -ang demang.tif -sca demsca.tif -nc
 
 ## Skeleton
 slopearea -slp demslp.tif -sca demsca.tif -sa demsa.tif
+## TODO: d8flowpathextremeup takes input ultimately from areadinf not aread8
+## TODO: possibly requires lumping step before d8flowpathextremeup
 d8flowpathextremeup -p demp.tif -sa demsa.tif -ssa demssa.tif -nc
 #python3 hand-thresh.py --resolution demfel.tif --output demthresh.txt
 #threshold -ssa demssa.tif -src demsrc.tif -thresh $(cat demthresh.txt)
+## TODO: Possibly re-incorporate hand-thresh.py or actually do a parameter sweep / sensitivity analysis
 threshold -ssa demssa.tif -src demsrc.tif -thresh 500.0
 
 streamnet -fel demfel.tif -p demp.tif -ad8 demad8.tif -src demsrc.tif -ord demord.tif -tree demtree.dat -coord demcoord.dat -net demnet.shp -w demw.tif -sw
